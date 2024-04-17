1c7892d93b3c770ebd01d30c23a91819e95584b0
Erenyeager88 <saad.ammad2005@gmail.com>
2024-04-17 20:44:29 +0500

    not completed

--- index.js
+++ index.js
@@ -70,9 +70,9 @@
 // display it’s multiplication table on your browser. If user 
 // does not enter a new number, multiplication table of 5 
 // should be displayed by default.
-var table = prompt("Enter a number for multiplication table");
-var t;
-var i;
+// var table = prompt("Enter a number for multiplication table");
+// var t;
+// var i;
 // if(i!==0){
 // for (i =1;i<=10;i++){
 //     result=table*i;
@@ -245,3 +245,201 @@ var i;
 // d is displayed
 // e is displayed
 // f is not displayed
+
+// QUES#06:
+// // Function to calculate the grade and remarks based on the percentage
+// function getGradeAndRemarks(percentage) {
+//     let grade = '';
+//     let remarks = '';
+  
+//     if (percentage >= 80) {
+//       grade = 'A-one';
+//       remarks = 'Excellent';
+//     } else if (percentage >= 70) {
+//       grade = 'A';
+//       remarks = 'Good';
+//     } else if (percentage >= 60) {
+//       grade = 'B';
+//       remarks = 'You need to improve';
+//     } else {
+//       grade = 'Fail';
+//       remarks = 'Sorry';
+//     }
+  
+//     return { grade, remarks };
+//   }
+  
+//   // Function to calculate the percentage and display the marks sheet
+//   function displayMarksSheet(marksObtained, totalMarks) {
+//     const percentage = (marksObtained / totalMarks) * 100;
+  
+//     console.log(`
+//   Marks Sheet
+//   ------------
+//   Total marks: ${totalMarks}
+//   Marks obtained: ${marksObtained}
+//   Percentage: ${percentage.toFixed(2)}%
+//   Grade: ${getGradeAndRemarks(percentage).grade}
+//   Remarks: ${getGradeAndRemarks(percentage).remarks}
+//   `);
+//   }
+  
+//   // Example usage:
+//   const totalMarks = 300;
+//   const marksObtained = 219;
+  
+//   displayMarksSheet(marksObtained, totalMarks);
+
+
+// CHAPTER 14-16:
+
+// 1. Declare an empty array using JS literal notation to store
+// student names in future.
+// 2. Declare an empty array using JS object notation to store
+// student names in future.
+// 3. Declare and initialize a strings array.
+// 4. Declare and initialize a numbers array.
+// 5. Declare and initialize a boolean array.
+// 6. Declare and initialize a mixed array.
+// 7. Declare and Initialize an array and store available
+// education qualifications in Pakistan (e.g. SSC, HSC, BCS,
+// BS, BCOM, MS, M. Phil., PhD). Show the listed
+// qualifications in your browser like:
+
+// let arr = [];// 1 done
+// let arr1 = {}//2 done
+// let arr3 = ["ali","ahmed"]//3 done
+// let arr4 = [1,2,3,4,5]//4 done
+// let arr5 = [true,false]//5 done
+// let arr6 = ["ali",1,true]//6 done
+
+// let arr7 =["HSC","SSC","BCS","BCOM","PHD","MS","BS"]
+// for(var x = 0 ;x < arr7.length; x++){
+//     document.write(arr7[x], "<br>");
+// }//7 done
+
+
+// 8. Write a program to store 3 student names in an array.Take
+// another array to store score of these three students.
+// Assume that total marks are 500 for each student, display
+// the scores & percentages of students like:
+
+// let students= ["ALI","HAMDAN","HASSAN"];
+// let marks = [495,365,489];
+// for(let i = 0 ; i<students.length; i++){
+//     document.write("Score of ",students[i]," is ",marks[i],". Percentage: ",(marks[i]/500)*100,"% <br>")
+// }
+
+
+// 9. Initialize an array with color names. Display the array
+// elements in your browser.
+// a. Ask the user what color he/she wants to add to the
+// beginning & add that color to the beginning of the array.
+// Display the updated array in your browser.
+// b. Ask the user what color he/she wants to add to the end
+// & add that color to the end of the array. Display the
+// updated array in your browser.
+// c. Add two more color to the beginning of the array.
+// Display the updated array in your browser.
+// d. Delete the first color in the array. Display the updated
+// array in your browser.
+// e. Delete the last color in the array. Display the updated
+// array in your browser.
+// f. Ask the user at which index he/she wants to add a color
+// & color name. Then add the color to desired
+// position/index. . Display the updated array in your
+// browser.
+// g. Ask the user at which index he/she wants to delete
+// color(s) & how many colors he/she wants to delete. Then remove the same number of color(s) from user-defined
+// position/index. . Display the updated array in your
+// browser.
+
+// let colors = ["red", "green", "blue", "yellow", "black"];
+
+// let addColor = prompt("Enter a color which you want to add in the begining of an array");
+// if(!addColor){
+// }else{
+//     colors.unshift(addColor);
+//     document.write("Updated array: ",colors,"<br>");
+// }//part a done
+
+// let addInLast = prompt("Enter a color which you want to add in the begining of an array");
+// if(!addInLast){
+// }else{
+//     colors.push(addInLast);
+//     document.write("Updated array: ",colors,"<br>");
+// }//part b done
+
+// for (var i = 0 ; i < 2; i++){
+//     let addColor = prompt(Enter a color which you want to add in the begining of an array ${i+1});
+//     if(!addColor){
+
+//     }else{
+//         colors.unshift(addColor)
+//     }
+// }
+// document.write("Updated array: ",colors,"<br>");//part c done
+
+// colors.shift()
+// document.write("Updated array: ",colors,"<br>");//part d done
+
+// colors.pop();
+// document.write("Updated array: ",colors,"<br>");//part e done
+
+
+// let index = prompt("Enter the index where you want to add a color");
+// let colorName = prompt("Enter the name of the color you want to add");
+// colors.splice(index, 0, colorName);
+// document.write("Updated array: " + colors + "<br>");//part f done
+
+// let startIndex = +prompt("Enter a starting index btwn 0 and 4");
+// if (startIndex >= 0 && startIndex < 5) {
+//     let endIndex = +prompt("Enter a ending index btwn 0 and 5");
+//     if(startIndex >= 0 && startIndex < 5){
+//         colors.splice(startIndex, endIndex + 1)
+//         document.write("Updated array: " + colors + "<br>");
+//     }else{
+//         document.write("Invalid Index")
+//     }
+    
+// }else{
+//     document.write("Invalid Index")
+// }//part g done
+
+// 11. Write a program to initialize an array with city names.
+// Copy 3 array elements from cities array to selectedCities
+// array.
+// let cities = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix"];
+// let selectedCities = cities.slice(1,4);
+// document.write("CITIES:",cities)
+// document.write("<br>")
+// document.write("SELECTED CITIES:",selectedCities)
+
+// 12. Write a program to create a single string from the
+// below mentioned array:
+// var arr = [“This ”, “ is ”, “ my ”, “ cat”];
+// (Use array’s join method)
+// var arr = ["this "," is "," my "," cat "];
+// document.write("ARRAY: ",arr.join());
+// document.write("<br></br>");
+// document.write("STRING: ",arr.join(""));
+
+
+// 13. Create a new array. Store values one by one in such a way
+// that you can access the values in the order in which they
+// were stored. (FIFO-First In First Out)
+// var devices = ["keyboard","mouse","printer","monitor"];
+// document.write("Devices: ",devices,"<br>");
+// for(var i = 0 ; i < devices.length; i++){
+//     document.write("OUT","<br>",devices[i],"<br>")
+// }
+
+// 15. Write a program to store phone manufacturers (Apple,
+//     Samsung, Motorola, Nokia, Sony & Haier) in an array.
+//     Display the following dropdown/select menu in your
+//     browser using document.write() method:
+// var phoneManufacturers = ["Apple","Samsung","Motorola","Nokia","Sony","Haier"];
+// document.write("<select>");
+// for(var i = 0 ; i<phoneManufacturers.length; i++){
+//     document.write("<option>"+phoneManufacturers[i]+"</option>");
+// }
